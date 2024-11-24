85588d75df8c344d0ad1fdd961f81181880a549b
Umesh Gurjar <95861754+Umyagurjar@users.noreply.github.com>
2024-11-24 14:10:05 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -8,7 +8,7 @@
 
 """
 Apache License 2.0
-Copyright (c) 2022 @Digital_Botz
+Copyright (c) 2022 @UmyaGurjar
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -26,8 +26,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 Telegram Link : https://t.me/Digital_Botz 
-Repo Link : https://github.com/DigitalBotz/Digital-Rename-Bot
-License Link : https://github.com/DigitalBotz/Digital-Rename-Bot/blob/main/LICENSE
+Repo Link : https://github.com/umyagurjar/FileRenameBot
+License Link : https://github.com/Umyagurjar/FileRenameBot/blob/main/LICENSE
 """
 
 # database imports
